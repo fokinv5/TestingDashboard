f4feb6eb640760a56064d6d7f56cdd71b8fd4404
Varvara Fokina <25864646@stu.mmu.ac.uk>
2026-08-23 01:04:23 +0100

    minor changes

--- main.py
+++ main.py
@@ -118,9 +118,11 @@ tokenised = word_tokenize(blog_data)
 tagged_tokens = pos_tag(tokenised)
 
 for word, tag in tagged_tokens:
-    if len(word) > 2:
+    if len(word) > 3:
         articles_lem.append(
             lemmatizer.lemmatize(word, get_wordnet_pos(tag)))
+    else :
+        articles_lem.append(word)
 tokenised = word_tokenize(','.join(articles_lem))
 
 #wordcloud eda----------------------------------------------------------------------------------------------------------
