1f6e368ecd3aefa821448d7228a252dc57fa166c
Varvara Fokina <25864646@stu.mmu.ac.uk>
2026-08-23 01:01:12 +0100

    minor changes

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ tokenised = word_tokenize(blog_data)
 tagged_tokens = pos_tag(tokenised)
 
 for word, tag in tagged_tokens:
+    if len(word) > 3:
         articles_lem.append(
             lemmatizer.lemmatize(word, get_wordnet_pos(tag)))
 tokenised = word_tokenize(','.join(articles_lem))
